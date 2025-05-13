2dcdf78aaf9829d0a1ad3d331f86b3befbe0819e
roydesha <roydeshar@gmail.com>
2025-05-13 14:21:22 -0400

    new controller

--- src/main/java/com/example/roydeshaandtwylablackjack/HelloApplication.java
+++ src/main/java/com/example/roydeshaandtwylablackjack/HelloApplication.java
@@ -15,7 +15,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("boardView.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 600, 400);
-        stage.setTitle("Hello!");
+        stage.setTitle("BLACKJACK!");
         stage.setScene(scene);
         stage.show();
         File file = new File("2_of_clubs.png");
--- src/main/java/com/example/roydeshaandtwylablackjack/Player.java
+++ src/main/java/com/example/roydeshaandtwylablackjack/Player.java
@@ -4,20 +4,24 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 
 public class Player {
-    @FXML
-    Button hitButton;
-    @FXML
-    Button standButton;
+    Button hitOne;
+    Button standOne;
+    Button hitTwo;
+    Button standTwo;
     Boolean playerHit;
+    int currentHandSum;
 
     public void initialize () {
+        //depending on who is player one and two set hit & stand to visable(false)
 
 
     }
 
     public void chooseHit () {
         if (playerHit = true) {
-
+            //code that allows players to be given a card from the deck
+        } else {
+            //currentHandSum stays the same
         }
 
     }
